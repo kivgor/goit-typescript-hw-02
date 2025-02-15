cd0a7fe7a51b744f7ca535e655d8cfa2d8329518
Igor <pa220282kiv@gmail.com>
2025-02-15 17:27:57 +0200

    Delete height in loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import css from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <RotatingLines height="96" width="96" strokeWidth="3" />
+      <RotatingLines width="96" strokeWidth="3" />
     </div>
   );
 };
